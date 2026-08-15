c7919e7f31707f9aa796ce2f3d318e0350b96ee6
shinoda <84962608+sadgedog@users.noreply.github.com>
2026-08-16 06:56:39 +0900

    Feature/util crate (#19)
    * chore: separate util crate
    
    * chore
    
    * fix

--- mediumi/src/lib.rs
+++ mediumi/src/lib.rs
@@ -1,7 +1,7 @@
 //! Facade crate that re-exports all mediumi container and codec crates.
 //!
 //! See each sub-crate for usage examples:
-//! [`aac`], [`ac3`], [`crypto`] [`h264`], [`mp4`], [`mpeg2ts`]
+//! [`aac`], [`ac3`], [`crypto`] [`h264`], [`mp4`], [`mpeg2ts`], [`util`]
 
 pub use mediumi_aac as aac;
 pub use mediumi_ac3 as ac3;
@@ -9,3 +9,4 @@ pub use mediumi_crypto as crypto;
 pub use mediumi_h264 as h264;
 pub use mediumi_mp4 as mp4;
 pub use mediumi_mpeg2ts as mpeg2ts;
+pub use mediumi_util as util;
